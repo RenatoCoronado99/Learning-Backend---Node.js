9afdc51df84d32e97cba3c1a92a532848fdf6df5
Renato Coronado <20163002@aloe.ulima.edu.pe>
2020-07-31 12:31:05 -0500

    finished CodingApp

--- Coding App/frontend/src/app/pages/technology/technology.component.ts
+++ Coding App/frontend/src/app/pages/technology/technology.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HttpService } from 'src/app/services/http.service';
+import { Technology } from 'src/app/models/technology.model';
 
 @Component({
   selector: 'app-technology',
   templateUrl: './technology.component.html',
   styleUrls: ['./technology.component.css']
 })
-export class TechnologyComponent implements OnInit {
+export class TechnologyComponent implements OnInit 
+{
+  public technology: Technology = 
+  {
+    _id: '',
+    name: '',
+    description: '',
+    logo: '',
+    tags:[],
+    createdAt: null,
+    updatedAt: null
+  };
 
-  constructor() { }
+  constructor(private _activatedRoute: ActivatedRoute, private _httpService: HttpService) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void 
+  {
+    this._activatedRoute.params.subscribe(params =>
+      {
+        const id = params['id'];
+        this._httpService.getTechnology(id).subscribe((technology: Technology)=>
+        {
+          this.technology = technology['data'];
+        });
+      });
   }
 
 }
