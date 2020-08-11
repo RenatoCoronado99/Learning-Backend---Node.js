eb1510b518773abcef94f2929e75433bef7d48e6
Renato Coronado <20163002@aloe.ulima.edu.pe>
2020-08-11 01:59:31 -0500

    [ADD] Swagger implemented

--- MyFirstAPI/src/routes/index.js
+++ MyFirstAPI/src/routes/index.js
@@ -4,6 +4,9 @@ const helmet = require("helmet"); //Paquete que nos ayuda con brechas de segurid
 const compression = require("compression"); //Nos ayuda a comprimir las peticiones HTTP para que sea más rápido
 require("express-async-errors"); //Nos ayuda a capturar en un middleware las exepciones asíncronas de un middleware
 const { NotFoundMiddleware, ErrorMiddleware } = require('../middlewares'); //Middlewares de errores y status code.
+const swaggerUI = require('swagger-ui-express');
+const { SWAGGER_PATH } = require('../config');
+const swaggerDocument = require(SWAGGER_PATH);
 
 module.exports = function ({ HomeRoutes, UserRoutes, IdeaRoutes, CommentRoutes, AuthRoutes }) 
 {
@@ -22,6 +25,7 @@ module.exports = function ({ HomeRoutes, UserRoutes, IdeaRoutes, CommentRoutes, 
   apiRoutes.use("/comment", CommentRoutes);
   apiRoutes.use("/auth", AuthRoutes);
   router.use("/v1/api", apiRoutes);
+  router.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
   router.use(NotFoundMiddleware);
   router.use(ErrorMiddleware);
   return router;
